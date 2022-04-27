3be22872a8b660c6818f8712e1ffb8a879f47d35
Yakov-Bader <yakovbader@gmail.com>
2022-04-27 04:49:01 +0300

    sdf

--- main.py
+++ main.py
@@ -2,11 +2,22 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-#add git huks
+# add git huks
 
 from flask import Flask, request, render_template, jsonify
+from pip._internal.vcs import git
 
 app = Flask(__name__)
+
+
+@app.route('/git_update', methods=['POST'])
+def git_update():
+    repo = git.Repo('./onlineAuctionAPI')
+    origin = repo.remotes.origin
+    repo.create_head('main', origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull()
+    return '', 200
+
 
 @app.route('/')
 def hello_world():
