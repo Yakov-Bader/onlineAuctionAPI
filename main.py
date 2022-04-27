4ad76a118494c0870920d12ab0e3cc06c88762e9
Yakov-Bader <yakovbader@gmail.com>
2022-04-28 01:16:21 +0300

    conect to db

--- main.py
+++ main.py
@@ -4,16 +4,19 @@ from flask import Flask, request, render_template, jsonify
 from pip._internal.vcs import git
 import git
 from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
 
 app = Flask(__name__)
-password = " "
+
+project_folder = os.path.expanduser('C:/Users/Yakov/PycharmProjects/onlineAuction')
+load_dotenv(os.path.join(project_folder, '.env'))
+password = os.getenv("password")
 link = 'mongodb+srv://yakov:'+password+'@cluster0.irzzw.mongodb.net/myAuctionDB?retryWrites=true&w=majority'
 client = MongoClient(link)
 db = client.get_database('myAuctionDB')
-users = db.users
-users.insert_one({})  # to add user
-users.find({})
-users.find_one({})
+
+
 @app.route('/git_update', methods=['POST'])
 def git_update():
     repo = git.Repo('./onlineAuctionAPI')
@@ -25,6 +28,10 @@ def git_update():
 
 @app.route('/')
 def hello_world():
+
+   # users.find({})
+   # users.find_one({})
+
     return jsonify({"chaim shwartz 3": "yakov bader 2"})
 
 
@@ -33,6 +40,8 @@ def signup():
 
     info = request.args
     if info["password"] == info["password2"] and info["name"] and info["email"] and info["password"] and info["password2"]:
+        users = db.users
+        users.insert_one({'name': info["name"], 'email': info["email"], 'password':info["password"]})
         return jsonify({"status": "ok", "message": " welcome to {} {} ".format(info["name"], info["email"])})
     else:
         return jsonify({"status": "error", "message": "you are missing information"})
