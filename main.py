748f51f86d253415f4860542b03ba2dd5194b3cc
Yakov-Bader <yakovbader@gmail.com>
2022-04-28 13:46:56 +0300

    dfg

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 
 # connect to mongoDB, connect to socket.io
+import time
+
+from dotenv import load_dotenv
 from flask import Flask, request, render_template, jsonify
 from pip._internal.vcs import git
 import git
@@ -8,6 +11,8 @@ import os
 
 app = Flask(__name__)
 
+#project_folder = os.path.expanduser('onlineAuction')
+#load_dotenv(os.path.join(project_folder, '.env'))
 
 
 @app.route('/git_update', methods=['POST'])
@@ -19,7 +24,7 @@ def git_update():
     return '', 200
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/')
 def hello_world():
 
     return jsonify({"chaim shwartz 3": "yakov bader 2"})
@@ -29,13 +34,27 @@ def hello_world():
 async def signup():
     info = request.args
     if info["password"] == info["password2"] and info["name"] and info["email"] and info["password"] and info["password2"]:
-
-        return jsonify({"status": "ok", "message": " welcome to {} {} ".format(info["name"], info["email"])})
-    else:
         password = os.getenv("password")
         link = 'mongodb+srv://yakov:' + password + '@cluster0.irzzw.mongodb.net/myAuctionDB?retryWrites=true&w=majority'
         client = MongoClient(link)
         db = client.get_database('myAuctionDB')
         users = db.users
-        user = await users.find({})
-        return jsonify({"status": "error", "message": user})
+        print(await pushuser(users, info))
+        return jsonify({"status": "ok", "message": " welcome to {} {} ".format(info["name"], info["email"])})
+    else:
+        return jsonify({"status": "error", "message": "you are missing some arguments"})
+
+
+async def pushuser(users, info):
+    return users.insert_one({
+        'name': info["name"],
+        'email': info["email"],
+        'password': info["password"],
+        'sales': [],
+        'offers': [],
+        'saved': []
+    })
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000, debug=True)
