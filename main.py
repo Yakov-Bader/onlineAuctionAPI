b74faba9e6af9175d6afcd72936d9e4519fa1950
Yakov-Bader <yakovbader@gmail.com>
2022-04-28 14:44:41 +0300

    ret pas

--- main.py
+++ main.py
@@ -38,11 +38,11 @@ def signup():
     info = request.args
     if info["password"] == info["password2"] and info["name"] and info["email"] and info["password"] and info["password2"]:
         password = os.getenv("password")
+        return password
         link = 'mongodb+srv://yakov:' + password + '@cluster0.irzzw.mongodb.net/myAuctionDB?retryWrites=true&w=majority'
         client = MongoClient(link)
         db = client.get_database('myAuctionDB')
         users = db.users
-        return "sdfg"
         users.insert_one({
             'name': info["name"],
             'email': info["email"],
