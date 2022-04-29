362efc50944ca45693d9abc95f1e3002a8495fc2
Yakov-Bader <yakovbader@gmail.com>
2022-04-29 12:58:21 +0300

    got pas

--- main.py
+++ main.py
@@ -37,8 +37,7 @@ async def signup():
     # might need to change to form not args
     info = request.args
     if info["password"] == info["password2"] and info["name"] and info["email"] and info["password"] and info["password2"]:
-        password = os.environ.get(("password"))
-        return password
+        password = os.environ.get("password")
         link = 'mongodb+srv://yakov:' + password + '@cluster0.irzzw.mongodb.net/myAuctionDB?retryWrites=true&w=majority'
         client = MongoClient(link)
         db = client.get_database('myAuctionDB')
