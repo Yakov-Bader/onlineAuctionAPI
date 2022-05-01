ac2159e9965b3011350d7c441b7797743f409827
Yakov-Bader <yakovbader@gmail.com>
2022-05-01 19:02:29 +0300

    dfgh

--- inup.py
+++ inup.py
@@ -19,7 +19,7 @@ def signin(request):
 
 def signup(request):
     # might need to change to form not args
-    info = request.form
+    info = request.headers
     if info["password"] and info["name"] and info["email"]:
         password = os.environ.get("password")
         link = 'mongodb+srv://yakov:' + password + '@cluster0.irzzw.mongodb.net/myAuctionDB?retryWrites=true&w=majority'
