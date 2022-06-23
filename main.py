c630835db9c22c846e61d9ad5454dcc5cfaba482
Yakov-Bader <yakovbader@gmail.com>
2022-06-24 02:12:22 +0300

    dfgh

--- main.py
+++ main.py
@@ -109,5 +109,5 @@ def on_leave(data):
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='localhost', port=5000, debug=True)
-    # app.run(host='localhost', port=5000, debug=True)
+    # socketio.run(app, host='localhost', port=5000, debug=True)
+    app.run(host='localhost', port=5000, debug=True)
