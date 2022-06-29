c04ce0f80fb33d7ef45702110b6e214125ed1e7c
Yakov-Bader <yakovbader@gmail.com>
2022-06-29 10:19:05 +0300

    loc

--- main.py
+++ main.py
@@ -119,5 +119,5 @@ def on_leave(data):
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='localhost', port=5000, debug=True)
+    socketio.run(app,int(os.environ.get('PORT', 33507)), debug=True)
     # app.run(host='localhost', port=5000, debug=True)
