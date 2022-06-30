d19a32a5a4b63d5068948d3f5d8ef9cfb94c7223
Yakov-Bader <yakovbader@gmail.com>
2022-06-30 20:11:58 +0300

    chat funcs

--- main.py
+++ main.py
@@ -129,5 +129,5 @@ def on_leave(data):
 
 
 if __name__ == '__main__':
-    # socketio.run(app, port=int(os.environ.get('PORT', 5000)), debug=True)
-    app.run(host='localhost', port=5000, debug=True)
+    socketio.run(app, port=int(os.environ.get('PORT', 5000)), debug=True)
+    # app.run(host='localhost', port=5000, debug=True)
